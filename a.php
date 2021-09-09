b1c2c4ac07e4c4b3f4e8f97d2c61f5700272e485
aelw <89507166+aelw@users.noreply.github.com>
2021-09-09 12:12:03 +0900

    Create a.php

---
+++ a.php
@@ -0,0 +1,113 @@
+<html>
+
+    <title>Shivangi Joshi</title>
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
+
+  <style>
+   * {color:#000;text-decoration:none;word-break:keep-all}
+    html {background:#fafafa}
+    body {background:#fff;padding:15px;box-sizing:border-box;margin:0 auto;width:80%}
+@media (max-width:1024px) {
+body {width:100%}
+}
+    </style>
+  <body> 
+
+
+<style>
+	span {font-size:13px;color:#fafafa;margin:10px 0}
+span a {font-size:13px;color:#ccc}
+iframe {margin:0 !important}
+</style>
+
+	<form style='padding:0;margin:0' action="/search" method="get">
+		<input type="text" placeholder="Search" name="q" style="outline:none;width:100%;padding:30px !important;height:45px;border:0;box-sizing:border-box" required size="50" maxlength="255">
+	</form>
+	  
+<div style='width:100%'><script>
+u = "https://blog.kakaocdn.net/dn/bGTcrg/btrewfvSDZ0/9WNnkaZQhjYuEGrpC17ylK/img.jpg";
+a = "https://blog.kakaocdn.net/dn/bD2rqz/btreu9C6RvJ/rZDyLWvYJlb5eKv2toykV0/img.jpg";	
+document.write("\
+<div style='width:50%;display:inline-block;padding:5px;box-sizing:border-box'>\n\
+<a href='"+u+"?attach=1&knm=img.jpg'>\n\
+<div style='height:275px;width:100%;background:url(\""+u+"\");background-size:cover;background-position:center;background-repeat:no-repeat;border-radius:5px'></div>\n\
+</a>\n\
+</div>\n\
+	\n\
+<div style='width:50%;display:inline-block;float:right;padding:5px;box-sizing:border-box'>\n\
+<a href='"+a+"?attach=1&knm=img.jpg'>\n\
+<div style='height:275px;width:100%;background:url(\""+a+"\");background-size:cover;background-position:center;background-repeat:no-repeat;border-radius:5px'></div>\n\
+</a>\n\
+</div>");
+</script></div>
+
+	  <div style='width:100%'><script>
+u = "https://blog.kakaocdn.net/dn/YGTbF/btrexdq3TN6/zfaQXZKJZoMP0eOLp8aTPK/img.jpg";
+a = "https://blog.kakaocdn.net/dn/bGwOnE/btreqI0BEaa/DpmMhW6sXv5F31WUGtBNSk/img.jpg";	
+document.write("\
+<div style='width:50%;display:inline-block;padding:5px;box-sizing:border-box'>\n\
+<a href='"+u+"?attach=1&knm=img.jpg'>\n\
+<div style='height:275px;width:100%;background:url(\""+u+"\");background-size:cover;background-position:center;background-repeat:no-repeat;border-radius:5px'></div>\n\
+</a>\n\
+</div>\n\
+	\n\
+<div style='width:50%;display:inline-block;float:right;padding:5px;box-sizing:border-box'>\n\
+<a href='"+a+"?attach=1&knm=img.jpg'>\n\
+<div style='height:275px;width:100%;background:url(\""+a+"\");background-size:cover;background-position:center;background-repeat:no-repeat;border-radius:5px'></div>\n\
+</a>\n\
+</div>");
+</script></div>
+	  
+	  	  <div style='width:100%'><script>
+u = "https://blog.kakaocdn.net/dn/cPdOuo/btrexejbExm/WKVPRzFX2GPep2jBIFKAQ1/img.jpg";
+a = "https://blog.kakaocdn.net/dn/dA04eA/btrexdkhRPq/gISm9J9lAiKAbRFBNJyfOK/img.jpg";	
+document.write("\
+<div style='width:50%;display:inline-block;padding:5px;box-sizing:border-box'>\n\
+<a href='"+u+"?attach=1&knm=img.jpg'>\n\
+<div style='height:275px;width:100%;background:url(\""+u+"\");background-size:cover;background-position:center;background-repeat:no-repeat;border-radius:5px'></div>\n\
+</a>\n\
+</div>\n\
+	\n\
+<div style='width:50%;display:inline-block;float:right;padding:5px;box-sizing:border-box'>\n\
+<a href='"+a+"?attach=1&knm=img.jpg'>\n\
+<div style='height:275px;width:100%;background:url(\""+a+"\");background-size:cover;background-position:center;background-repeat:no-repeat;border-radius:5px'></div>\n\
+</a>\n\
+</div>");
+</script></div>
+
+	  	  	  <div style='width:100%'><script>
+u = "https://blog.kakaocdn.net/dn/VWtr2/btrewfvSD2t/2I99uhjM7KKkkKL7CUKhX1/img.jpg";
+a = "https://blog.kakaocdn.net/dn/csQ8qv/btrex0E0tgK/jeV03NjJI1xmEv8fqaVBbK/img.jpg";	
+document.write("\
+<div style='width:50%;display:inline-block;padding:5px;box-sizing:border-box'>\n\
+<a href='"+u+"?attach=1&knm=img.jpg'>\n\
+<div style='height:275px;width:100%;background:url(\""+u+"\");background-size:cover;background-position:center;background-repeat:no-repeat;border-radius:5px'></div>\n\
+</a>\n\
+</div>\n\
+	\n\
+<div style='width:50%;display:inline-block;float:right;padding:5px;box-sizing:border-box'>\n\
+<a href='"+a+"?attach=1&knm=img.jpg'>\n\
+<div style='height:275px;width:100%;background:url(\""+a+"\");background-size:cover;background-position:center;background-repeat:no-repeat;border-radius:5px'></div>\n\
+</a>\n\
+</div>");
+</script></div>
+	  
+	  	  	  <div style='width:100%'><script>
+u = "https://blog.kakaocdn.net/dn/dVY91r/btrepvApETd/F7WJEkGLYTC2LCvKzOKg61/img.jpg";
+a = "https://blog.kakaocdn.net/dn/bn0MEP/btrevQb3N9X/Ou4Hpj0mS80EfSyn7I9MC0/img.jpg";	
+document.write("\
+<div style='width:50%;display:inline-block;padding:5px;box-sizing:border-box'>\n\
+<a href='"+u+"?attach=1&knm=img.jpg'>\n\
+<div style='height:275px;width:100%;background:url(\""+u+"\");background-size:cover;background-position:center;background-repeat:no-repeat;border-radius:5px'></div>\n\
+</a>\n\
+</div>\n\
+	\n\
+<div style='width:50%;display:inline-block;float:right;padding:5px;box-sizing:border-box'>\n\
+<a href='"+a+"?attach=1&knm=img.jpg'>\n\
+<div style='height:275px;width:100%;background:url(\""+a+"\");background-size:cover;background-position:center;background-repeat:no-repeat;border-radius:5px'></div>\n\
+</a>\n\
+</div>");
+</script></div>
+	  </body>
+
