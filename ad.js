a104d9e14878108e58984f207d1549cc1d243ac9
aelw <89507166+aelw@users.noreply.github.com>
2021-10-03 11:49:27 +0900

    Update ad.js

--- ad.js
+++ ad.js
@@ -1,3 +1,3 @@
 document.write("\
-<!--<script async src=\"https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9733039422277496\"\n\
-     crossorigin=\"anonymous\"></","script>-->");
+<script async src=\"https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9733039422277496\"\n\
+     crossorigin=\"anonymous\"></","script>");
